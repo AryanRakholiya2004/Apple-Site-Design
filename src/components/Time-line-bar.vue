ecb1b1b6cfbcf3d373aa2ba69eacd2b8c216afa4
aryan rakholiya <aryanrakholiya@proton.me>
2025-10-14 15:45:13 +0530

    Fixed look of scroll-time-line bottom

--- src/components/Time-line-bar.vue
+++ src/components/Time-line-bar.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="scroll-progress-bar" :class="[
-            'fixed left-1/2 bottom-2 -translate-x-1/2 w-[90vw] max-w-[900px] h-6 pointer-events-none opacity-0 transition-opacity duration-400 ease-[cubic-bezier(.4,0,.2,1)] z-[1000] flex items-center justify-center',
+            'fixed left-1/2 bottom-2 -translate-x-1/2 w-[40vw] h-6 px-20 rounded-full bg-black bg-opacity-30 pointer-events-none opacity-0 liquid-glass px-20 transition-opacity duration-400 ease-[cubic-bezier(.4,0,.2,1)] z-[1000] flex items-center justify-center',
             isVisible ? 'opacity-100' : '',
         ]">
         <div
-            class="bar absolute left-0 right-0 top-1/2 h-0.5 bg-white rounded w-full -translate-y-1/2 shadow-[0_0_8px_0_rgba(255,255,255,0.08)]">
+            class="bar absolute left-50 right-50 top-1/2 h-0.5 bg-white rounded w-[95%] -translate-y-1/2 shadow-[0_0_8px_0_rgba(255,255,255,0.08)]">
         </div>
         <div class="circle absolute top-1/2 w-3 h-3 bg-white rounded-full shadow-[0_0_16px_4px_rgba(255,255,255,0.4),0_2px_8px_0_rgba(0,0,0,0.12)] -translate-y-1/2 transition-[left] duration-250 ease-[cubic-bezier(.4,0,.2,1)]"
             :style="{ left: circleLeft + '%' }"></div>
